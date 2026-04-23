f57f9394737fcb3b315ff5fd2b46fc6ee2db3596
ZXPrism <2897544036@qq.com>
2026-04-23 20:16:09 +0800

    v2.1.0

--- tsup.config.ts
+++ tsup.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     js: `// ==UserScript==
 // @name         Tieba Post Backup Tool
 // @namespace    https://github.com/ZXPrism/TiebaPostBackupTool
-// @version      2.0.2
+// @version      2.1.0
 // @description  Automatically backup Tieba posts in one single click
 // @author       ZXPrism
 // @license      MIT
